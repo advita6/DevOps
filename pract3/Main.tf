057b224358fbacb9a2b44425bd18d534f7552639
Advita <advita@gmail.com>
2026-01-29 21:32:06 +0530

    Replace real AWS keys with demo credentials

--- pract3/Main.tf
+++ pract3/Main.tf
@@ -11,9 +11,10 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAV3KAI4NFLXUJV5XM"
-  secret_key = "eS2GdHBtxrWFzEJQsi8Dl+vPLYk01kXOzMcZJKmi"
+  access_key = "AKIAFAKEKEY1234567890"
+  secret_key = "fakeSecretKeyForTerraformDemoOnly123456"
 }
+
 
 resource "aws_instance" "ec2" {
   ami           = "ami-0c02fb55956c7d316"
