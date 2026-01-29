512143432ef0d02c53b9dcac63d1f0a3e308a133
Advita <advita@gmail.com>
2026-01-29 16:57:12 +0530

    Added Terraform EC2 config in pract3

---
+++ pract3/Main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.92"
+    }
+  }
+
+  required_version = ">= 1.2"
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAV3KAI4NFLXUJV5XM"
+  secret_key = "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"
+}
+
+resource "aws_instance" "ec2" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Variable-EC2"
+  }
+}
